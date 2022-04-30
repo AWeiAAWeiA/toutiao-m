02d49645b0ffa440a196df57a149956cd15c3ab0
AWeiAAWeiA <shenchangweizs@163.com>
2022-04-30 13:08:44 +0800

    项目初始化，引入vant组件库

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+Vue.use(Vant);
+Vue.config.productionTip=false
+
 
 Vue.use(VueRouter)
 
